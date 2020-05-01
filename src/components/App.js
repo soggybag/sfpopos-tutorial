bb6ea29460a1b1bb2d347ac640f893923f4d4eac
Mitchell <soggybag@gmail.com>
2020-05-01 12:04:18 -0700

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -25,3 +25,37 @@ function App() {
 }
 
 export default App;
+
+const p = [[1,2], [1,3], [2,5], [4, 5]]
+
+function distance(p1, p2) {
+  const dx = p1[0] - p2[0]
+  const dy = p1[1] - p2[1]
+
+  return Math.sqrt(dx * dx + dy * dy)
+}
+
+const d = []
+for (let i = 0; i < p.length; i += 1) {
+  for (let j = i; j < p.length; j += 1) {
+    if (i !== j) {
+      // console.log(i, j)
+      // console.log(p[i], p[j])
+      const l = { id: `p${i}${j}`, dist: distance(p[i], p[j]) }
+      d.push(l)
+      console.log(l)
+    }
+  }
+}
+
+
+
+d.sort((a, b) => {
+  return a.dist - b.dist
+})
+
+console.log(d)
+
+
+
+
